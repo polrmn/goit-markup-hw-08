669cd4736e4b22e26bc6a8e2bed89a0e95af3309
polrmn <polrmn@gmail.com>
2022-11-17 17:37:57 +0200

    <768px viewPort

--- mobile-menu.js
+++ mobile-menu.js
@@ -1,14 +1,17 @@
 (() => {
   const menuBtnRef = document.querySelector("[data-menu-button]")
-  const mobileMenuRef = document.querySelector("[data-menu]");
+  const mobileMenuRef = document.querySelector("[data-menu]")
+  const bodyMenuRef = document.querySelector ("[data-menu-body]");
   menuBtnRef.addEventListener('click', () => {
     const expanded =
-      menuBtnRef.getAttribute("aria-expanded") === "true" || false;
+    menuBtnRef.getAttribute("aria-expanded") === "true" || false;
     
     menuBtnRef.classList.toggle("is-open");
     menuBtnRef.setAttribute("aria-expended", !expanded);
 
     mobileMenuRef.classList.toggle("is-open");
+
+    bodyMenuRef.classList.toggle("is-open");
 
   })
 })();
